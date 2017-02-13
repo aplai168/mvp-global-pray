4c337b356e4d5801eb37f559121503609aec35d7
Alice Lai <aplai168@gmail.com>
2017-02-13 16:47:05 -0600

    add prayer controller and services

--- code/server/server.js
+++ code/server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 const mongoose = require('mongoose');
-var PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost/prayer', (err, database) => {
+const PORT = process.env.PORT || 5000;
+
+// this is for any middlewares I need to use
+require('./config/middleware.js')(app, express);
+
+require('./config/routes.js')(app, express);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.end('hey!');
+});
+
+mongoose.connect('mongodb://localhost/prayer', (err) => {
   if (err) {
     console.log(err);
   }
   app.listen(PORT, () => {
-    console.log('Connected to Mongoose database and listening at localhost:' + PORT);
+    console.log("Connected to Mongoose database and listening at localhost:" + PORT);
   });
 });
--- code/server/users/userModel.js
+++ code/server/users/userModel.js
@@ -1,54 +1,55 @@
 const mongoose = require('mongoose');
 const Q = require('q');
 const bcrypt = require('bcrypt-nodejs');
-var Schema = mongoose.Schema;
-var saltFactor = 10;
 
-var UserSchema = new Schema({
+const Schema = mongoose.Schema;
+const saltFactor = 10;
+
+const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    require: true
+    require: true,
   },
-  salt: String
+  salt: String,
 });
 
-UserSchema.methods.comparePasswords = function(candidatePassword) {
-  var savedPassword = this.password;
+UserSchema.methods.comparePasswords = (candidatePassword) => {
+  const savedPassword = this.password;
   return Q.Promise((resolve, reject) => {
     bcrypt.compare(candidatePassword, savedPassword, (err, boolean) => {
-      if(err) {
-        reject(err)
+      if (err) {
+        reject(err);
       } else {
         resolve(boolean);
       }
     });
   });
-}
-//pre is a middleware as opposed to post
+};
+// pre is a middleware as opposed to post
 UserSchema.pre('save', (next) => {
-  var user = this;
-  //has the password if new or different
-  if(!user.isModified('password')) {
-    //pass it forward if not modified
+  const user = this;
+  // has the password if new or different
+  if (!user.isModified('password')) {
+    // pass it forward if not modified
     return next();
   }
   bcrypt.genSalt(saltFactor, (err, salt) => {
     if (err) {
       return next(err);
     }
-    //hash the password along with the salt
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) {
-        return next(err);
+    // hash the password along with the salt
+    bcrypt.hash(user.password, salt, null, (error, hash) => {
+      if (error) {
+        return next(error);
       }
       user.password = hash;
       user.salt = salt;
-      next();
+      return next();
     });
   });
 });
